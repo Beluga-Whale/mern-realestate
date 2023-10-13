ceecaec54a3801a558d4f3cbb0292356a207ec10
Thanathat <thanathat.j@houseofdev.tech>
2023-10-13 14:56:57 +0700

    update header search

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,8 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../store';
 import { userSelector } from '../store/user/userSlice';
+import { useEffect, useState } from 'react';
 
 type User = {
     currentUserGoogle?: {
@@ -12,6 +13,27 @@ type User = {
 
 const Header = () => {
     const user: User = useAppSelector(userSelector) as User;
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const navigate = useNavigate();
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
 
     return (
         <header className="shadow-md bg-[#EBE4D1] ">
@@ -22,13 +44,20 @@ const Header = () => {
                         <span className="text-[#C63D2F]">Estate</span>
                     </h1>
                 </Link>
-                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                <form
+                    onSubmit={handleSubmit}
+                    className="bg-slate-100 p-3 rounded-lg flex items-center"
+                >
                     <input
                         type="text"
                         placeholder="Search..."
                         className="bg-transparent focus:outline-none w-24 sm:w-64"
+                        value={searchTerm}
+                        onChange={handleSearch}
                     />
-                    <FaSearch className="text-slate-600" />
+                    <button>
+                        <FaSearch className="text-slate-600" />
+                    </button>
                 </form>
                 <ul className="flex gap-4">
                     <Link to="/">
